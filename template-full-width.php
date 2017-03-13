ca96f343da5c152ca21c6c5ea0776363f51afc34
Stefan <iftimie.s@gmail.com>
2017-03-13 16:49:48 +0200

    Bug gix

--- template-full-width.php
+++ template-full-width.php
@@ -18,7 +18,8 @@ get_header(); ?>
 			<?php
 			while ( have_posts() ) : the_post();
 
-				get_template_part( 'template-parts/content', 'page' );
+				$content_type = ( 'post' === get_post_type() ) ? 'single' : 'page';
+				get_template_part( 'template-parts/content', $content_type );
 
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
